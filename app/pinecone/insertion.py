7fda4470aa539517d63610b40c3081de0be02e06
daniyal <114733266+d4niy4l@users.noreply.github.com>
2025-04-16 00:48:30 +0500

    fixed docker build

--- app/pinecone/insertion.py
+++ app/pinecone/insertion.py
@@ -3,7 +3,7 @@ from app.embeddings.generate_embeddings import generate_embeddings
 
 def insert_item(food_data):
     pinecone = get_pinecone_client()
-    embeddings = generate_embeddings(f"{food_data['name']} {food_data['description']} {food_data["ingredients"]} {food_data["nutrient_description"]}")
+    embeddings = generate_embeddings(f"{food_data['name']} {food_data['description']} {food_data['ingredients']} {food_data['nutrient_description']}")
     index = pinecone.Index("items")
     index.upsert(
         vectors=[(str(food_data["item_id"]), embeddings, food_data)],
